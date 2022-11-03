1663a814978801a7b4fae1ac8e750d1efb75c2ea
rdev32 <rdevelops32@gmail.com>
2022-11-02 21:09:45 -0500

    prototipado de funciones

--- e1.py
+++ e1.py
@@ -1,5 +1,3 @@
-pass       
-
 class Libro:
     def __init__(self, id, titulo, genero, ISBN, editorial, autors):
         self.id = id
@@ -23,5 +21,35 @@ class Libro:
     def __del__(self):
         pass
         
+def leer_libro():
+    pass
+
+def listar_libros():
+    pass
+
+def agregar_libro():
+    pass
+
+def eliminar_libro():
+    pass
+
+def ordenar_libros():
+    pass
+
+def buscar_libro_por_isbn_titulo():
+    pass
+
+def buscar_libro_por_autor_editorial_genero():
+    pass
+
+def buscar_libro_por_no_autores():
+    pass
+
+def actualizar_libro():
+    pass
+
+def guardar_libros():
+    pass
+
 if __name__ == "__main__":
     pass
