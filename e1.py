7147a9d0134c7202c0fad85dc8546bdb401a6083
JNC <th3y@hotmail.com>
2022-11-03 11:13:06 -0500

    Correcion de la clase Libro

--- e1.py
+++ e1.py
@@ -1,23 +1,38 @@
 class Libro:
-    def __init__(self, id, titulo, genero, ISBN, editorial, autors):
-        self.id = id
-        self.titulo = titulo
-        self.genero = genero
-        self.ISBN = ISBN
-        self.editorial = editorial
-        self.autor = autors
+    def __init__(self, id, titulo, genero, isbn, editorial, autores):
+        self.__id = id
+        self.__titulo = titulo
+        self.__genero = genero
+        self.__isbn = isbn
+        self.__editorial = editorial
+        self.__autores = autores
+        
+    def set_id(self, value):
+        self.__id = value
+    def set_titulo(self, value):
+        self.__titulo = value
+    def set_genero(self, value):
+        self.__genero = value
+    def set_isbn(self, value):
+        self.__isbn = value
+    def set_editorial(self, value):
+        self.__editorial = value
+    def set_autores(self, value):
+        self.__autores = value
 
-    def set_titulo(self, titulo):
-        pass
-    def set_genero(self, titulo):
-        pass
-    def set_isbn(self, titulo):
-        pass
-    def set_editorial(self, titulo):
-        pass
-    def set_autores(self, titulo):
-        pass
-
+    def get_id(self):
+        return self.__id
+    def get_titulo(self):
+        return self.__titulo
+    def get_genero(self):
+        return self.__genero
+    def get_isbn(self):
+        return self.__isbn
+    def get_editorial(self):
+        return self.__editorial
+    def get_autores(self):
+        return self.__autores
+    
     def __del__(self):
         pass
         
