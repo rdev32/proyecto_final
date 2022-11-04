a0b9be06bf1b57ee14b8228de79caf67d05bd90d
rdev32 <rdevelops32@gmail.com>
2022-11-03 20:14:26 -0500

    modularizar la clase libro

---
+++ book.py
@@ -0,0 +1,22 @@
+class Libro:
+    def __init__(self, id, titulo, genero, ISBN, editorial, autors):
+        self.id = id
+        self.titulo = titulo
+        self.genero = genero
+        self.ISBN = ISBN
+        self.editorial = editorial
+        self.autor = autors
+
+    def set_titulo(self, titulo):
+        pass
+    def set_genero(self, titulo):
+        pass
+    def set_isbn(self, titulo):
+        pass
+    def set_editorial(self, titulo):
+        pass
+    def set_autores(self, titulo):
+        pass
+
+    def __del__(self):
+        pass
