f2e2f4fd439a5804a05a881d53a655c61adb4a95
rdev32 <rdevelops32@gmail.com>
2022-11-04 15:15:31 -0500

    revisando contenidos para mejor prototipado

---
+++ ejercicio_2.py
@@ -0,0 +1,18 @@
+# Generar los menus desde request
+def listar_por_generacion():
+    pass
+
+def listar_por_forma(*args, **kargs):
+    pass
+
+def listar_por_habilidad():
+    pass
+
+def listar_por_habitad():
+    pass
+
+def listar_por_tipo():
+    pass
+
+if __name__ == "__main__":
+    pass
