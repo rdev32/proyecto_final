3c5f07717c162280c431d737e3a59d7ba7f2f970
rdev32 <rdevelops32@gmail.com>
2022-11-06 23:13:15 -0500

    implementacion de opciones 1 y 2

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,9 +1,23 @@
-# Generar los menus desde request
-def listar_por_generacion():
-    pass
+import requests
 
-def listar_por_forma(*args, **kargs):
-    pass
+def listar_por_generacion(int_gen: int) -> list:
+    req = requests.get("https://pokeapi.co/api/v2/generation/" + str(int_gen))
+    with req as data:
+        lista = []
+        json = data.json()
+        for result in json["pokemon_species"]:
+            lista.append( result["url"].split("/")[-2] )
+        return list(set(lista))
+
+def listar_por_forma(shape_str: str) -> list:
+    req = requests.get("https://pokeapi.co/api/v2/pokemon-shape/" + shape_str)
+    with req as data:
+        lista = []
+        json = data.json()
+
+        for result in json["pokemon_species"]:
+            lista.append( result["url"].split("/")[-2] )
+        return list(set(lista))
 
 def listar_por_habilidad():
     pass
